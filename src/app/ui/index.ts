32b1387ff86e80a2be231b47ec54b6422e617b63
Khalil LAGRIDA <khalil.lagrida@gmail.com>
2026-02-10 23:34:56 +0100

    wip

--- src/app/ui/index.ts
+++ src/app/ui/index.ts
@@ -1,5 +1,1 @@
-export { ScTabs } from './tabs';
-export { ScTabList, tabsListVariants } from './tab-list';
-export { ScTab } from './tab';
-export { ScTabPanel } from './tab-panel';
-export { ScTabContent } from './tab-content';
+export { ScTabs, ScTabList, ScTab, ScTabPanel, ScTabContent, tabsListVariants } from './tabs';
