4543347a6e7e9edeaa0b6c0abeb1ccca9395e422
Khalil LAGRIDA <khalil.lagrida@gmail.com>
2026-02-11 11:10:17 +0100

    wip

--- src/app/ui/accordion/accordion-content.ts
+++ src/app/ui/accordion/accordion-content.ts
@@ -1,6 +1,8 @@
 import { Directive } from '@angular/core';
+import { AccordionContent } from '@angular/aria/accordion';
 
 @Directive({
   selector: 'ng-template[scAccordionContent]',
+  hostDirectives: [AccordionContent],
 })
 export class ScAccordionContent {}
--- src/app/ui/accordion/accordion-panel.ts
+++ src/app/ui/accordion/accordion-panel.ts
@@ -1,23 +1,11 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  contentChild,
-  inject,
-  input,
-  TemplateRef,
-  ViewEncapsulation,
-} from '@angular/core';
-import { NgTemplateOutlet } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input, ViewEncapsulation } from '@angular/core';
 import { AccordionPanel } from '@angular/aria/accordion';
 import { cn } from '../../utils';
-import { ScAccordionContent } from './accordion-content';
 
 @Component({
   selector: '[scAccordionPanel]',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
-  imports: [NgTemplateOutlet],
   hostDirectives: [
     {
       directive: AccordionPanel,
@@ -29,18 +17,12 @@ import { ScAccordionContent } from './accordion-content';
     '[class]': 'class()',
   },
   template: `
-    @if (panel.visible()) {
-      <div class="overflow-hidden" animate.enter="animate-accordion-down" animate.leave="animate-accordion-up">
-        <div class="pt-0 pb-2.5">
-          <ng-container [ngTemplateOutlet]="contentTemplate()" />
-        </div>
-      </div>
-    }
+    <div class="pt-0 pb-2.5">
+      <ng-content />
+    </div>
   `,
 })
 export class ScAccordionPanel {
-  readonly panel = inject(AccordionPanel);
-  readonly contentTemplate = contentChild.required(ScAccordionContent, { read: TemplateRef });
   readonly classInput = input<string>('', { alias: 'class' });
-  readonly class = computed(() => cn('text-sm', this.classInput()));
+  readonly class = computed(() => cn('text-sm [&[inert]]:hidden', this.classInput()));
 }
