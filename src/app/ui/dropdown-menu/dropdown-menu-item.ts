4b008c86e7b402fe533c464a5516c2a8611ff259
Khalil LAGRIDA <khalil.lagrida@gmail.com>
2026-02-11 19:36:30 +0100

    wip

--- src/app/ui/dropdown-menu/dropdown-menu-item.ts
+++ src/app/ui/dropdown-menu/dropdown-menu-item.ts
@@ -3,9 +3,10 @@ import { MenuItem } from '@angular/aria/menu';
 import { cn } from '../../utils';
 
 export const dropdownMenuItemBaseStyles = [
+  'data-[active=true]:bg-accent data-[active=true]:text-accent-foreground',
   'focus:bg-accent focus:text-accent-foreground',
-  'data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 dark:data-[variant=destructive]:focus:bg-destructive/20 data-[variant=destructive]:focus:text-destructive data-[variant=destructive]:*:[svg]:text-destructive',
-  'not-data-[variant=destructive]:focus:**:text-accent-foreground',
+  'data-[variant=destructive]:text-destructive data-[variant=destructive]:data-[active=true]:bg-destructive/10 dark:data-[variant=destructive]:data-[active=true]:bg-destructive/20 data-[variant=destructive]:data-[active=true]:text-destructive data-[variant=destructive]:*:[svg]:text-destructive',
+  'not-data-[variant=destructive]:data-[active=true]:**:text-accent-foreground',
   'gap-1.5 rounded-md px-1.5 py-1 text-sm data-inset:pl-7',
   '[&_svg:not([class*=size-])]:size-4',
   'group/dropdown-menu-item relative flex cursor-default items-center outline-hidden select-none',
--- src/app/ui/dropdown-menu/dropdown-menu.ts
+++ src/app/ui/dropdown-menu/dropdown-menu.ts
@@ -23,6 +23,7 @@ export class ScDropdownMenu {
   readonly classInput = input<string>('', { alias: 'class' });
   readonly class = computed(() =>
     cn(
+      'data-[visible=false]:hidden',
       'bg-popover text-popover-foreground min-w-32 rounded-lg p-1 shadow-md ring-1 ring-foreground/10 z-50 overflow-x-hidden overflow-y-auto',
       this.classInput(),
     ),
