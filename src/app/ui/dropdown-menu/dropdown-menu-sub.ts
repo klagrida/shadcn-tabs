9a6e00b1d2f93f480ee75ba77a17cfe479eab3ec
Khalil LAGRIDA <khalil.lagrida@gmail.com>
2026-02-11 19:34:28 +0100

    wip

--- src/app/ui/dropdown-menu/dropdown-menu-sub.ts
+++ src/app/ui/dropdown-menu/dropdown-menu-sub.ts
@@ -23,6 +23,7 @@ export class ScDropdownMenuSub {
   readonly classInput = input<string>('', { alias: 'class' });
   readonly class = computed(() =>
     cn(
+      'data-[visible=false]:hidden',
       'bg-popover text-popover-foreground min-w-24 rounded-md p-1 shadow-lg ring-1 ring-foreground/10 z-50 overflow-hidden',
       this.classInput(),
     ),
